b9cd3f8dda5cef09ffe0567d4807124420d21a08
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-10-28 08:54:16 +0430

    Add Article table

---
+++ db/migrate/20231028042301_create_article.rb
@@ -0,0 +1,10 @@
+class CreateArticle < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
